a4d011e85f0591d44018482cc1967789ad94fe7b
Guilherme <guilherme.oliver@msn.com>
2024-10-14 13:43:06 -0300

    third commit

--- ApresentacaoGIT.c
+++ ApresentacaoGIT.c
@@ -2,135 +2,172 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define MAX 150
+int qnt = 0;
 
 struct doador {
-    char nome[MAX], email[MAX], senha[MAX], rg[MAX], cpf[MAX], telefone[MAX];
-};
+    char nome[100], email[100], senha[100], rg[12], cpf[14], telefone[19];
+} doador_pessoa[100];
 
 void clean() {
     while (getchar() != '\n');
 }
 
+// Função para remover o '\n' das strings lidas pelo fgets
+void remove_newline(char *str) {
+    size_t len = strlen(str);
+    if (len > 0 && str[len - 1] == '\n') {
+        str[len - 1] = '\0';
+    }
+}
 
-void negocinhodecamila(){
-    struct doador doador_pessoa;
-    char senha_confirmacao[MAX], resposta;
+void rgg() {
+    while(1) {
+        printf("\nRG (Ex: xx.xxx.xxx-x):\n");
+        scanf("%12s", doador_pessoa[qnt].rg);
+        clean();
+        if (strlen(doador_pessoa[qnt].rg) > 10) {
+            printf("=============== Cadastro do doador - pessoa ===============\n\n");
+            printf("Voce excedeu a quantidade de caracteres. Tente novamente.\n");
+        } else {
+            break;
+        }
+    }
+}
+
+void cpff() {
+    while(1) {
+        printf("\nCPF (Ex: xxx.xxx.xxx-xx):\n");
+        scanf("%14s", doador_pessoa[qnt].cpf);
+        clean();
+        if (strlen(doador_pessoa[qnt].cpf) > 11) {
+            printf("=============== Cadastro do doador - pessoa ===============\n\n");
+            printf("Voce excedeu a quantidade de caracteres. Tente novamente.\n");
+        } else {
+            break;
+        }
+    }
+}
+
+void telefonee() {
+    while(1) {
+        printf("\nTelefone (Ex: (xx) xxxxx-xxxx):\n");
+        scanf("%19s", doador_pessoa[qnt].telefone);
+        clean();
+        if (strlen(doador_pessoa[qnt].telefone) > 14) {
+            printf("=============== Cadastro do doador - pessoa ===============\n\n");
+            printf("Voce excedeu a quantidade de caracteres. Tente novamente.\n");
+        } else {
+            break;
+        }
+    }
+}
+
+void negocinhodecamila() {
+    char senha_confirmacao[100], resposta;
     int opcao;
 
     printf("=============== Cadastro do doador - pessoa ===============\n\n");
 
     printf("Nome completo:\n");
-    fgets(doador_pessoa.nome, MAX, stdin);
+    fgets(doador_pessoa[qnt].nome, 100, stdin);
+    remove_newline(doador_pessoa[qnt].nome);
 
     printf("\nE-mail:\n");
-    fgets(doador_pessoa.email, MAX, stdin);
+    fgets(doador_pessoa[qnt].email, 100, stdin);
+    remove_newline(doador_pessoa[qnt].email);
 
-    // Loop para confirmação da senha
     while (1) {
         printf("\nSenha:\n");
-        fgets(doador_pessoa.senha, MAX, stdin);
+        fgets(doador_pessoa[qnt].senha, 100, stdin);
+        remove_newline(doador_pessoa[qnt].senha);
 
         printf("\nConfirmacao da senha:\n");
-        fgets(senha_confirmacao, MAX, stdin);
+        fgets(senha_confirmacao, 100, stdin);
+        remove_newline(senha_confirmacao);
 
-        if (strcmp(doador_pessoa.senha, senha_confirmacao) == 0){
+        if (strcmp(doador_pessoa[qnt].senha, senha_confirmacao) == 0) {
             printf("Senha confirmada com sucesso!\n");
             break;
         } else {
-            system("cls");
-            printf("=============== Cadastro do doador - pessoa ===============\n\n");
             printf("Senhas nao coincidem. Tente novamente.\n");
         }
     }
 
-    printf("\nRG:\n");
-    fgets(doador_pessoa.rg, MAX, stdin);
+    rgg();
+    cpff();
+    telefonee();
 
-    printf("\nCPF:\n");
-    fgets(doador_pessoa.cpf, MAX, stdin);
-
-    printf("\nTelefone:\n");
-    fgets(doador_pessoa.telefone, MAX, stdin);
-
-    do{
-
+    do {
         system("cls");
-        printf("=============== Cadastro do doador - pessoa ===============\n");
+        printf("\n=============== Cadastro do doador - pessoa ===============\n\n");
         printf("                     Dados cadastrados:\n\n");
-        printf("Nome: %s", doador_pessoa.nome);
-        printf("E-mail: %s", doador_pessoa.email);
-        printf("RG: %s", doador_pessoa.rg);
-        printf("CPF: %s", doador_pessoa.cpf);
-        printf("Telefone: %s", doador_pessoa.telefone);
-
-        printf("\nVoce gostaria de alterar algum dado? (s/n): ");
+        printf("Nome: %s\n", doador_pessoa[qnt].nome);
+        printf("E-mail: %s\n", doador_pessoa[qnt].email);
+        printf("RG: %s\n", doador_pessoa[qnt].rg);
+        printf("CPF: %s\n", doador_pessoa[qnt].cpf);
+        printf("Telefone: %s\n\n", doador_pessoa[qnt].telefone);
+        printf("Voce gostaria de alterar algum dado? (s/n): ");
         scanf(" %c", &resposta);
         clean();
 
-        if(resposta == 's' || resposta == 'S'){
-
+        if (resposta == 's' || resposta == 'S') {
             system("cls");
             printf("=============== Cadastro do doador - pessoa ===============\n\n");
             printf("              Qual dado voce deseja alterar?\n\n");
-            printf("(1) Nome: %s", doador_pessoa.nome);
-            printf("(2) E-mail: %s", doador_pessoa.email);
-            printf("(3) RG: %s", doador_pessoa.rg);
-            printf("(4) CPF: %s", doador_pessoa.cpf);
-            printf("(5) Telefone: %s\n", doador_pessoa.telefone);
+            printf("(1) Nome: %s\n", doador_pessoa[qnt].nome);
+            printf("(2) E-mail: %s\n", doador_pessoa[qnt].email);
+            printf("(3) RG: %s\n", doador_pessoa[qnt].rg);
+            printf("(4) CPF: %s\n", doador_pessoa[qnt].cpf);
+            printf("(5) Telefone: %s\n\n", doador_pessoa[qnt].telefone);
             printf("Escolha uma opcao (1-5): ");
             scanf("%d", &opcao);
             clean();
 
-            switch(opcao){
-
+            switch(opcao) {
                 case 1:
                     system("cls");
                     printf("=============== Cadastro do doador - pessoa ===============\n\n");
                     printf("Novo nome:\n");
-                    fgets(doador_pessoa.nome, MAX, stdin);
+                    fgets(doador_pessoa[qnt].nome, 100, stdin);
+                    remove_newline(doador_pessoa[qnt].nome);
                     break;
                 case 2:
                     system("cls");
                     printf("=============== Cadastro do doador - pessoa ===============\n\n");
                     printf("Novo e-mail:\n");
-                    fgets(doador_pessoa.email, MAX, stdin);
+                    fgets(doador_pessoa[qnt].email, 100, stdin);
+                    remove_newline(doador_pessoa[qnt].email);
                     break;
                 case 3:
                     system("cls");
                     printf("=============== Cadastro do doador - pessoa ===============\n\n");
-                    printf("Novo RG:\n");
-                    fgets(doador_pessoa.rg, MAX, stdin);
+                    rgg();
                     break;
                 case 4:
                     system("cls");
                     printf("=============== Cadastro do doador - pessoa ===============\n\n");
-                    printf("Novo CPF:\n");
-                    fgets(doador_pessoa.cpf, MAX, stdin);
+                    cpff();
                     break;
                 case 5:
                     system("cls");
                     printf("=============== Cadastro do doador - pessoa ===============\n\n");
-                    printf("Novo telefone:\n");
-                    fgets(doador_pessoa.telefone, MAX, stdin);
+                    telefonee();
                     break;
                 default:
                     printf("Opção inválida!\n");
-                    system("Pause");
-
+                    system("pause");
             }
         }
 
     } while (resposta == 's' || resposta == 'S');
 
-    system("cls");
     printf("\nCadastro realizado com sucesso!\n\n");
     printf("Dados do Doador:\n\n");
-    printf("Nome: %s", doador_pessoa.nome);
-    printf("E-mail: %s", doador_pessoa.email);
-    printf("RG: %s", doador_pessoa.rg);
-    printf("CPF: %s", doador_pessoa.cpf);
-    printf("Telefone: %s", doador_pessoa.telefone);
+    printf("Nome: %s\n", doador_pessoa[qnt].nome);
+    printf("E-mail: %s\n", doador_pessoa[qnt].email);
+    printf("RG: %s\n", doador_pessoa[qnt].rg);
+    printf("CPF: %s\n", doador_pessoa[qnt].cpf);
+    printf("Telefone: %s\n", doador_pessoa[qnt].telefone);
 }
 
 int main() {
